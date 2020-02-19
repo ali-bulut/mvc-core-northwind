81ea359ae184895b0c7fb903c5d5fe9dd751b1fe
Ali Bulut <alibulut@yahoo.com>
2020-02-19 18:33:13 +0300

    refactoring process was set up

---
+++ NorthwindBackend/Business/Contants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Contants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product was successfully added!";
+        public static string ProductUpdated = "Product was successfully updated!";
+        public static string ProductDeleted = "Product was successfully deleted!";
+
+    }
+}
---
+++ NorthwindBackend/Core/Utilities/Results/SuccessDataResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class SuccessDataResult<T>:DataResult<T>
+    {
+        public SuccessDataResult(T data, string message) : base(data, true, message)
+        {
+        }
+
+        public SuccessDataResult(T data) : base(data, true)
+        {
+        }
+
+        public SuccessDataResult(string message) : base(default, true, message)
+        {
+
+        }
+
+        //default; referans tipler için null, int için 0, boolean için ise false döner.
+        public SuccessDataResult():base(default,true)
+        {
+
+        }
+    }
+}
