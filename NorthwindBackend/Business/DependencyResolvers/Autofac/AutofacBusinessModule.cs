9d8839697c7b9b0ba4465b3486f563296caf361f
Ali Bulut <alibulut@yahoo.com>
2020-02-19 18:59:48 +0300

    dependency injection operations were created by using Autofac

---
+++ NorthwindBackend/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //startupta yaptığımız dependency injection'ı business'a taşıdık çünkü
+            //yarın bir gün web api yerine direkt mvc projesinde kullanılması istenirse
+            //bir daha uğraşmamıza gerek kalmaz.
+            //autofac'i web apiye tanıtmamız için web api projesi altında program.cs'de işlem yaparız.
+            //Web api içerisine autofac.dependency package'ını nuget'tan yüklemek lazım!
+            //eğer birisi ctor'da IProductService isterse ona ProductManager'ı ver.
+            builder.RegisterType<ProductManager>().As<IProductService>();
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+
+        }
+    }
+}
