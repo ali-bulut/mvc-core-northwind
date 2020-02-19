eba5044ea03ddea96efe83001f548af4c36186f6
Ali Bulut <alibulut@yahoo.com>
2020-02-19 17:39:57 +0300

    DataAccess layer was prepared

---
+++ NorthwindBackend/DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(localdb)\MSSQLLocalDB; Database=Northwind; Integrated Security=True;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
