07351157e9d9bc1be168204b1634448c4ac18d62
Ali Bulut <alibulut@yahoo.com>
2020-02-22 16:02:52 +0300

    method interception infrastructure was created

---
+++ NorthwindBackend/Core/Utilities/Interceptors/Autofac/MethodInterception.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Castle.Core.Interceptor;
+
+namespace Core.Utilities.Interceptors.Autofac
+{
+    public abstract class MethodInterception:MethodInterceptionBaseAttribute
+    {
+        //OnBefore -> Method çalışmadan önce sen çalış demek
+        protected virtual void OnBefore(IInvocation invocation) { }
+        //OnAfter -> Method çalıştıktan sonra sen çalış demek
+        protected virtual void OnAfter(IInvocation invocation) { }
+        //OnException -> Method hata verdiğinde sen çalış demek
+        protected virtual void OnException(IInvocation invocation) { }
+        //OnSuccess -> Method başarılıysa sen çalış demek
+        protected virtual void OnSuccess(IInvocation invocation) { }
+
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+            OnBefore(invocation);
+            try
+            {
+                //invocation methodunu çalıştır demek.
+                invocation.Proceed();
+            }
+            catch (Exception e)
+            {
+                isSuccess = false;
+                OnException(invocation);
+                throw;
+            }
+            finally
+            {
+                if (isSuccess)
+                {
+                    OnSuccess(invocation);
+                }
+            }
+
+            OnAfter(invocation);
+        }
+    }
+}
---
+++ NorthwindBackend/Core/Utilities/Interceptors/Autofac/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Castle.Core.Interceptor;
+
+namespace Core.Utilities.Interceptors.Autofac
+{
+    //IInterceptor'ı kullanmamız için castle.dynamicproxy package'ını kurmamız lazım.
+    //classlar'da, methodlar'da ve istenirse bir method için birden fazla ve son olarak inherit edildiği alt classlarda
+    //kullanılabilir.
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Method,AllowMultiple = true,Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute:Attribute,IInterceptor
+    {
+        //attribute'lar yukarıdan aşağıya doğru çalışır fakat biz bu işin sırasını kendimiz belirlemek istiyoruz.
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+}
